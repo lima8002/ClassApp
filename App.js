770eb7c517589de2137be9c7255ad6fb05d451de
Eduardo Lima <lima8002@gmail.com>
2020-08-06 23:17:17 +1000

    ClickCounter

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 // custom component
 import { TextBox } from './components/TextBox';
+import { ClickCounter } from './components/ClickCounter';
 
 export default function App() {
   return (
@@ -10,8 +11,10 @@ export default function App() {
       <Text style={styles.greeting}>Click Example</Text>
       <Button title="Click me" color="blue" />
       <StatusBar style="auto" />
-      <TextBox color="green" size="24" text="Custom Component" />
-      <TextBox color="purple" size="18" text="Custom TextBox 2" />
+      <TextBox color="green" size={24} text="Custom Component" />
+      <TextBox color="purple" size={18} text="Custom TextBox 2" />
+      <ClickCounter/>
+      <ClickCounter/>
     </View>
   );
 }
