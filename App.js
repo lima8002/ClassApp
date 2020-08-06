26a0f129c11bcaded4de7eeb6e0ab7342dba9110
Eduardo Lima <lima8002@gmail.com>
2020-08-06 22:32:23 +1000

    Second commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi Eduardo</Text>
+      <Text style={styles.greeting}>Click Example</Text>
+      <Button title="Click me" color="blue" />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +18,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  greeting: {
+    color: 'red',
+    fontSize: 36
   },
 });
